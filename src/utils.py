3ed441dc96424cff3d4dd5cade44b0e3be1f9865
greg <gregory.erickson@tbs-sct.gc.ca>
2025-01-31 21:19:16 +0000

    fix drf-related measure tables, reorganized modules

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import re
 
 from src.load import load_csv_from_raw
 from src.export import export_to_csv
-from src.clean import standardize_column_names
+from src.clean import standardize_column_names, clean_fiscal_yr
 
 UTILS_DIR = Path(__file__).parent.parent / "outputs" / "utils"
 
@@ -44,6 +44,110 @@ def dept_list():
     )
     
     return dept
+    
+
+def sid_list(si):
+    # Unique list of service IDs as reported in the latest fy
+    sid_list = si.loc[
+        si.groupby('service_id')['fiscal_yr'].idxmax(), [
+            'service_id', 
+            'service_name_en', 
+            'service_name_fr', 
+            'fiscal_yr', 
+            'department_en', 
+            'department_fr', 
+            'org_id'
+        ]
+    ]
+
+    export_to_csv(
+        data_dict={'sid_list': sid_list},
+        output_dir=UTILS_DIR
+    )
+
+    return sid_list
+
+
+def build_drf():
+    # Load and clean DRF data (i.e. RBPO)
+    drf = load_csv_from_raw('rbpo.csv')
+    drf = standardize_column_names(drf)
+    drf['fiscal_yr'] = drf['fiscal_yr'].apply(clean_fiscal_yr)
+    
+    # Define columns related to planned and actual measures: spending and FTEs 
+    fte_spend_cols = [
+        'planned_spending_1', 
+        'actual_spending', 
+        'planned_spending_2', 
+        'planned_spending_3',
+        'planned_ftes_1', 
+        'actual_ftes', 
+        'planned_ftes_2', 
+        'planned_ftes_3'
+    ]
+    
+    # Melt (unpivot) the DataFrame to long format
+    drf = pd.melt(
+        drf, 
+        id_vars=['fiscal_yr', 'org_id', 'program_id'], 
+        value_vars=fte_spend_cols, 
+        var_name='plan_actual_spendfte_yr', 
+        value_name='measure'
+    )
+    
+    # Split 'plan_actual_yr' into separate columns for planned/actual, spending/FTEs, and year adjustment
+    drf[['planned_actual', 'spending_fte', 'yr_adjust']] = drf['plan_actual_spendfte_yr'].str.split('_', expand=True)
+    drf['yr_adjust'] = drf['yr_adjust'].fillna('1').astype(int) - 1
+    
+    # Calculate 4-digit 'measure_yr' and 'report_yr' from 'fiscal_yr' and 'yr_adjust'
+    drf['measure_yr'] = drf['fiscal_yr'].str.split('-').str[1].astype(int) + drf['yr_adjust']
+    drf['report_yr'] = drf['fiscal_yr'].str.split('-').str[1].astype(int)
+    
+    # Get the latest fiscal year from the Service inventory (four digit fy, year of end of fy)
+    # latest_si_fy = si['fiscal_yr'].str.split('-').str[1].astype(int).max()
+    latest_si_fy = 2024
+    
+    # Separate actuals and future planned data
+    drf_actuals = drf[
+        (drf['planned_actual'] == 'actual') & 
+        (drf['report_yr'] <= latest_si_fy)
+    ].dropna()
+    
+    drf_planned = drf[
+        (drf['planned_actual'] == 'planned') &
+        (drf['report_yr'] > latest_si_fy) 
+    ].dropna()
+    
+    # Each report year has 3 measure years for planned values.
+    # Only keep records that have the highest report year for that given program, measure type, and measure year
+    idx = drf_planned.groupby(['program_id', 'spending_fte', 'measure_yr'])['report_yr'].idxmax()
+    drf_planned = drf_planned.loc[idx]
+    
+    # Concatenate actuals and planned entries
+    drf = pd.concat([drf_actuals, drf_planned])
+        
+    # Pivot to get a wide format table with spending/FTE columns
+    drf = drf.pivot_table(
+        index=['org_id', 'program_id', 'report_yr', 'measure_yr', 'planned_actual'], 
+        columns=['spending_fte'], 
+        values='measure'
+    ).sort_values(
+        by=['org_id', 'program_id', 'report_yr','measure_yr']
+    ).reset_index()
+       
+    # Set up si_link_yr: a fiscal year column to be able to include years 
+    # beyond the service inventory when joining by service id and fy.
+    # if measure year > latest service fy, = latest service fy, else use measure_yr
+    drf.loc[drf['measure_yr']>latest_si_fy, 'si_link_yr'] = latest_si_fy
+    drf.loc[drf['measure_yr']<=latest_si_fy, 'si_link_yr'] = drf['measure_yr']
+    drf['si_link_yr'] = drf['si_link_yr'].astype(int) 
+
+    export_to_csv(
+        data_dict={'drf': drf},
+        output_dir=UTILS_DIR
+    )
+
+    return drf
 
 def copy_raw_to_utils():
     ifoi_en = load_csv_from_raw('ifoi_en.csv')
