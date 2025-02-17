adb9f7eaaa398c038521daaa729c0575e50df7bd
greg e <gregory.erickson@tbs-sct.gc.ca>
2025-02-17 15:44:30 +0000

    updating output tests

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -12,15 +12,18 @@ def test_fields():
     ref_file = REF_DIR / "reference_fields.csv"
     reference_fields = pd.read_csv(ref_file)
 
-    paths = reference_fields['file_path'].unique()
+    file_names = reference_fields['file_name'].unique()
     
-    for path in paths:
+    for file_name in file_names:
+        path = OUTPUT_DIR / file_name
         df = pd.read_csv(path, delimiter=";", skipfooter=1, engine='python')  
         df_inferred = df.convert_dtypes()  # Infer data types from all rows
-        df['breaking!'] = 1
-        col_set_reference = set(reference_fields['fields'][reference_fields['file_path'] == path])
+        # df['breaking!'] = 1
+
+        col_set_reference = reference_fields['fields'][reference_fields['file_name'] == file_name]
+        col_set_reference = set(col_set_reference)
         col_set_test = set(df_inferred.columns)
-        assert col_set_test == col_set_reference, "missing column!"            
+        assert col_set_test == col_set_reference, "missing column!"
       
 
 if __name__ == "__main__":
