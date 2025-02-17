fbf036ff2ae897e1395a8cdc3b0b0934098428eb
greg e <gregory.erickson@tbs-sct.gc.ca>
2025-02-17 15:44:30 +0000

    updating output tests

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -8,21 +8,46 @@ import pytest
 OUTPUT_DIR = Path(__file__).parent.parent / "outputs"
 REF_DIR = Path(__file__).parent / "ref"
 
-def test_fields():
+def test_no_columns_dropped():
     ref_file = REF_DIR / "reference_fields.csv"
     reference_fields = pd.read_csv(ref_file)
 
-    paths = reference_fields['file_path'].unique()
+    file_names = reference_fields['file_name'].unique()
     
-    for path in paths:
+    for file_name in file_names:
+        path = OUTPUT_DIR / file_name
+        df = pd.read_csv(path, delimiter=";", skipfooter=1, engine='python')
+
+        col_set_reference = reference_fields['fields'][reference_fields['file_name'] == file_name]
+        col_set_reference = set(col_set_reference)
+        col_set_test = set(df.columns)
+        
+        assert col_set_test == col_set_reference, "missing column!"
+
+def test_column_types():    
+    ref_file = REF_DIR / "reference_fields.csv"
+    reference_fields = pd.read_csv(ref_file)
+
+    file_names = reference_fields['file_name'].unique()
+
+    for file_name in file_names:
+        path = OUTPUT_DIR / file_name
         df = pd.read_csv(path, delimiter=";", skipfooter=1, engine='python')  
         df_inferred = df.convert_dtypes()  # Infer data types from all rows
-        df['breaking!'] = 1
-        col_set_reference = set(reference_fields['fields'][reference_fields['file_path'] == path])
-        col_set_test = set(df_inferred.columns)
-        assert col_set_test == col_set_reference, "missing column!"            
-      
+
+        actual_dtypes = df_inferred.dtypes.astype(str).to_dict()
+        expected_dtypes = dict(zip(reference_fields['fields'][reference_fields['file_name']==file_name], reference_fields['datatype'][reference_fields['file_name']==file_name]))
+
+        mismatches = {
+            col: (actual_dtypes[col], expected_dtypes[col])
+            for col in expected_dtypes
+            if actual_dtypes.get(col) != expected_dtypes[col]
+            }
+
+        assert not mismatches, f'Datatype mismatches: {mismatches}'
+                  
 
 if __name__ == "__main__":
-    test_fields()
+    test_no_columns_dropped()
+    test_column_types()
     
