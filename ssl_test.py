36377fbbc24aa9bfc6f1b5d16a4a5af5b22fc001
greg e <gregory.erickson@tbs-sct.gc.ca>
2026-02-23 13:57:07 +0000

    tidying up repo

--- ssl_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-#'2018-2019':'https://donnees-data.tpsgc-pwgsc.gc.ca/ba1/cp-pc/cp-pc-1819-eng.csv',
-
-url = 'https://donnees-data.tpsgc-pwgsc.gc.ca/ba1/cp-pc/cp-pc-1819-eng.csv'
-requests.get(url)
